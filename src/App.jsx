bf557ed657ff74589da426145498f6eef8633db9
JesusJr <Jesusujr101@gmail.com>
2024-02-13 01:51:36 -0500

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '../src/assets/Header';
+import Nav from '../src/assets/Nav';
+import Footer from '../src/assets/Footer';
+import { Outlet } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Header className='headerNav'>
+      <Nav />
+    </Header>
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
+
+export default App;
